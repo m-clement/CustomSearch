2d362e207bf7dccec2b9ab548c8b32e08fe16702
Miguel Clement <miguelclement@gmail.com>
2023-11-23 22:10:25 -0500

    Playing around with search for company data

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import requests
 
-API_KEY = open('API_KEY').read()
-SEARCH_ENGINE_ID = open('SEARCH_ENGINE_ID').read()
+def get_ceo_image(company):
+    API_KEY = open('API_KEY').read().strip()
+    SEARCH_ENGINE_ID = open('SEARCH_ENGINE_ID').read().strip()
 
-search_query = 'roman empire'
+    query = f'{company} CEO'
 
-url = 'https://www.googleapis.com/customsearch/v1'
-params = {
-    'q': search_query,
-    'key': API_KEY,
-    'cx': SEARCH_ENGINE_ID,
-    'searchType': 'images'
-}
+    url = 'https://www.googleapis.com/customsearch/v1'
+    params = {
+        'q': query,
+        'key': API_KEY,
+        'cx': SEARCH_ENGINE_ID,
+        'searchType': 'image'
+    }
 
-response = requests.get(url, params=params)
-results = response.json()
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
+        results = response.json()
 
-if 'items' in results:
-    print(results['items'][0]['link'])
+        if 'items' in results:
+            return results['items'][0]['link']
+        else:
+            return "No results found"
+    except requests.RequestException as e:
+        return f"An error occurred: {e}"
+
+# Example usage
+company = input('Enter a company name: ')
+print(get_ceo_image(company))
